4fa71e8f4f3d0eb270c4d64f799168ea889bcb45
Osama Jandali <osama.jandali@robovics.com>
2018-01-27 23:43:20 +0200

    styling home and login

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,5 +1,39 @@
 import React from 'react'
+import glamorous from 'glamorous'
 
+const Input = glamorous.input({
+  background: 'white',
+  height: 50,
+  border: 'none',
+  borderRadius: 10,
+  boxShadow: 'var(--shadow)',
+  borderBottom: 5,
+  width: '30%',
+  display: 'block',
+  margin: '0 auto 10px auto',
+  paddingLeft: 10,
+  '::placeholder': {
+    opacity: 0.5,
+  },
+  '@media only screen and (max-width: 744px)': {
+    width: '70%',
+  },
+})
+
+const Button = glamorous.input({
+  background: 'var(--green)',
+  padding: '10px 20px',
+  display: 'block',
+  margin: 'auto',
+  color: 'white',
+  borderRadius: 10,
+  boxShadow: 'var(--shadow)',
+  cursor: 'pointer',
+  transition: '0.5s',
+  ':hover': {
+    boxShadow: 'var(--shadowHover)',
+  },
+})
 function Login({onSubmit}) {
   return (
     <div>
@@ -14,20 +48,18 @@ function Login({onSubmit}) {
           })
         }}
       >
-        <div>
-          <label>
-            username <input name="username" data-test="username-input" />
-          </label>
-        </div>
-        <div>
-          <label>
-            password{' '}
-            <input type="password" name="password" data-test="password-input" />
-          </label>
-        </div>
-        <button type="submit" data-test="login-submit">
-          Submit
-        </button>
+        <Input
+          placeholder="Username..."
+          name="username"
+          data-test="username-input"
+        />
+        <Input
+          placeholder="Password..."
+          type="password"
+          name="password"
+          data-test="password-input"
+        />
+        <Button type="submit" data-test="login-submit" />
       </form>
     </div>
   )
